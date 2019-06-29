16594cff7df485b7af903bb300a3e03c229906be
Yuki Koyama <yuki@koyama.xyz>
2019-06-30 00:36:24 +0900

    Add a minimum Python file

---
+++ split-toning.py
@@ -0,0 +1,26 @@
+bl_info = {
+    "name": "split-toning",
+    "author": "Yuki Koyama",
+    "version": (0, 0),
+    "blender": (2, 79, 0),
+    "location": "",
+    "description":
+    "Simulating the Split Toning effect in Adobe Lightroom/Photoshop",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Render"
+}
+
+
+def register():
+    print("split-toning: registered.")
+
+
+def unregister():
+    print("split-toning: unregistered.")
+
+
+if __name__ == "__main__":
+    register()
